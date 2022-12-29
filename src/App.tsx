31feb372a7a71f82e7ba4300486b6561db1a064a
DG <gdavidcefim@gmail.com>
2022-12-29 16:54:21 +0100

    Prepare router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FormPage } from './components/pages/form';
 
-function App() {
-    return <div className="App"></div>;
-}
+const App = () => {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<FormPage />} />
+                    {/* <Route path="/datatable" element={<Table />} /> */}
+                    {/* {<Route path="*" element={<Page404 />} /> */}
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+};
 
 export default App;
